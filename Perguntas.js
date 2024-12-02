64a009fa3ede55c9c3630b1c812841dcc7424048
PerfiEstudante <168859501+PerfiEstudante@users.noreply.github.com>
2024-12-02 13:16:42 -0300

    Create Perguntas.js

---
+++ Perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'softwares',
+    'O que é softwares?',
+    'Um software é um serviço computacional utilizado para realizar ações nos sistemas de computadores'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da Italia, Paris'
+)
+
+criaCartao(
+    'Engenharia',
+    'Qual seria o conceito da Engenharia?',
+    'Conjunto de técnicas e métodos para aplicar o conhecimento técnico e científico na planificação, criação e manutenção de estruturas, máquinas e sistemas para benefício do ser humano'
+)
+
+criaCartao(
+    'Lingua Francesa',
+    'Como se diz maça em Frances?',
+    'Maça em frances é Pomme'
+)
